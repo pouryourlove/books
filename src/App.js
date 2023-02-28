7bab94efb590412583a1d314177ce53ab13f6989
Juyeon Lee <lqu134@naver.com>
2023-02-28 23:24:19 +0900

    random id

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    const updatedBooks = [...books, { id: 123, title }];
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
     setBooks(updatedBooks);
   };
   return (

61394bdab5d00aff8f57e955ec73834a6c6f877c
Juyeon Lee <lqu134@naver.com>
2023-03-05 23:34:40 +0900

    delete works

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function BookShow({ book, onDelete }) {
-  return <div className="book-show">{book.title}</div>;
+  const handleClick = () => {
+    onDelete(book.id);
+  };
+  return (
+    <div className="book-show">
+      {book.title}
+      <div className="actions">
+        <button className="delete" onClick={handleClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }

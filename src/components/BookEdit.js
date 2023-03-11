91e83faf70470b71939ef8312c9aeefecd2c088b
Juyeon Lee <lqu134@naver.com>
2023-03-11 23:41:30 +0900

    making it as one props

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
-export default function BookEdit({ book, onEdit, onSubmit }) {
+export default function BookEdit({ book, onSubmit }) {
   const [title, setTitle] = useState(book.title);
 
   const handleChange = (event) => {
@@ -11,8 +11,7 @@ export default function BookEdit({ book, onEdit, onSubmit }) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    onEdit(book.id, title);
-    onSubmit();
+    onSubmit(book.id, title);
   };
   return (
     <form onSubmit={handleSubmit} className="book-edit">
